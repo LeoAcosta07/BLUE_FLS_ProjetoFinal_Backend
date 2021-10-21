6ee36068ad078963779dcb14a1f64515713d3928
LeoAcosta07 <leoacosta07@gmail.com>
2021-10-21 01:35:09 -0300

    0.3

--- conn/conn.js
+++ conn/conn.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Conn = () => {
-    mongoose.connect('mongodb+srv://dbUser:vJat44KTCA6D09Za@cluster0.i426i.mongodb.net/myTarefas?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }).then(() => {
+const Conn = (url,user,pass,data) => {
+   mongoose.connect(`${url}/${data}`, {
+         user: user,
+         pass: pass,
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+      })
+      .then(() => {
         console.log('MongoDB CONECTADO!')
-      }).catch((err) => console.log('Erro no mongoo', err));
+      })
+      .catch((err) => console.log('Erro no mongoo!!!!', err));
     }
     
     module.exports = Conn;
--- controllers/tarefas.controller.js
+++ controllers/tarefas.controller.js
@@ -6,55 +6,57 @@ const tarefasService = new TarefasServices();
 
 class TarefasController {
 
-    getTarefas = async (req, res) => {
-        const tarefas  = await tarefasService.findAll();
-        res.send(tarefas);
-      }
+  getTarefas = async (req, res) => {
+    const tarefas  = await tarefasService.findAll();
+    res.send(tarefas);
+  }
 
-      getTarefaById = async (req, res) => {
-        const id = req.params.id;
-    
-        if(!mongoose.Types.ObjectId.isValid(id)) {
-          res.status(403).send('Id Invalido');
-          return;
-        }
-    
-        const tarefa = await tarefaService.findById(id);
-    
+  getTarefaById = async (req, res) => {
+    const id = req.params.id;
 
-        if(!tarefa) {
-          res.status(404).send('A tarefa não existe');
-          return
-        }
-    
-        res.status(200).send(tarefa);
-      }
-    
-      createTarefa = async (req,res) => {
-        const tarefa = req.body;
-        const tarefaSalva = await tarefasService.createTarefa(tarefa)
-        .then(() => {
-          res.send({ message: `tarefa criada com sucesso` });
-        })
-        .catch((err) => res.status(500).send({error: `erro no servdor: ${err}`}));
-      }
-    
-      editTarefa = async (req, res) => {
-        const id = req.params.id;
-        const tarefa = req.body;
-        await tarefasService.editTarefa(id, tarefa)
-        .then(() => {
-          res.status(200).send({message: 'Tarefa atualizada com sucesso'});
-        })
-        .catch((err) => res.status(500).send({error: `erro no servdor: ${err}`}));
-      }
-    
-      deleteTarefa = async (req, res) => {
-        const id = req.params.id;
-        await tarefasService.deleteTarefa(id)
-        .then(() => res.status(200).send({message: 'Tarefa apagada corretamente'}));
-      }
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(403).send('Id Invalido');
+      return;
+    }
 
+    const tarefa = await tarefaService.findById(id);
+
+
+    if(!tarefa) {
+      res.status(404).send('A tarefa não existe');
+      return
+    }
+
+    res.status(200).send(tarefa);
+  }
+  
+  
+  createTarefa = async (req,res) => {
+    const tarefa = req.body;
+    const tarefaSalva = await tarefasService.createTarefa(tarefa)
+    .then(() => {
+      res.send({ message: `tarefa criada com sucesso` });
+    })
+    .catch((err) => res.status(500).send({error: `erro no servdor: ${err}`}));
+  }
+
+
+  editTarefa = async (req, res) => {
+    const id = req.params.id;
+    const tarefa = req.body;
+    await tarefasService.editTarefa(id, tarefa)
+    .then(() => {
+      res.status(200).send({message: 'Tarefa atualizada com sucesso'});
+    })
+    .catch((err) => res.status(500).send({error: `erro no servdor: ${err}`}));
+  }
+
+  deleteTarefa = async (req, res) => {
+    const id = req.params.id;
+    await tarefasService.deleteTarefa(id)
+    .then(() => res.status(200).send({message: 'Tarefa apagada corretamente'}));
+  }
+            
 }
 
 module.exports = TarefasController;
