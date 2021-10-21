a8c7560fb9cab55c4ab8a5b7206c0f50f6b37aef
LeoAcosta07 <leoacosta07@gmail.com>
2021-10-21 01:35:44 -0300

    0.3

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+  }
+
+
 // importar o express
 const express = require('express');
 // importar a biblioteca mongoonse que sera responsavel pelo mongodb (banco de dados)
@@ -7,6 +12,7 @@ const cors = require('cors');
 
 const Tarefa = require('./models/tarefas');
 
+
 const TarefasRoutes = require('./routes/tarefas.routes');
 const Conn = require('./conn/conn');
 
@@ -15,12 +21,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-Conn();
+const db_url= process.env.DB_URL;
+const db_user= process.env.DB_USER;
+const db_pass= process.env.DB_PASS;
+const db_data= process.env.DB_DATA;
+
+
+Conn(db_url, db_user, db_pass, db_data);
 
 app.use('/tarefas', TarefasRoutes);
 
 
 const port = 3001;
-app.listen(port, () => {
-  console.log(`App rodando em htttp://localhost:${port}/`);
-})
+app.listen(process.env.PORT || port, () => console.log(`App rodando em htttp://localhost:${port}/`));
